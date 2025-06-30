c85a13b4d74e69ef83bc8caff4b07a43e7f624f3
Sujindharsivakumar <sujisiva71@gmail.com>
2025-06-30 15:27:28 +0530

    Update and rename DoDoPlatform.java to Seleniumselector.java

--- Seleniumselector.java
+++ Seleniumselector.java
@@ -26,7 +26,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class DodoPlatform {
+public class Seleniumselector {
     WebDriver driver;
 
     @BeforeTest
