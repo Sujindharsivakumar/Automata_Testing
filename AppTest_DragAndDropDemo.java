e0ff7872c77d124b0d37c2fb85497a465bf72c42
Sujindharsivakumar <sujisiva71@gmail.com>
2025-06-30 15:37:17 +0530

    Update and rename DragAndDropDemo.java to AppTest_DragAndDropDemo.java

--- AppTest_DragAndDropDemo.java
+++ AppTest_DragAndDropDemo.java
@@ -27,7 +27,7 @@ import org.testng.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-public public class DragAndDropDemo {
+public public class AppTest_DragAndDropDemo {
     WebDriver driver;
 
     @BeforeTest
