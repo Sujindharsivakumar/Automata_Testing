8199b25afce60d33bc1afe070ef7f867870b1676
Sujindharsivakumar <sujisiva71@gmail.com>
2025-06-30 15:52:44 +0530

    Update AppTest_MouseHoverEvent.java

--- AppTest_MouseHoverEvent.java
+++ AppTest_MouseHoverEvent.java
@@ -1,3 +1,32 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+
+import javax.swing.Action;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Set;
+import org.bouncycastle.jcajce.provider.asymmetric.dsa.DSASigner.dsaRMD160;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 public class AppTest_MouseHoverEvent {
     WebDriver driver;
     WebDriverWait wait;
